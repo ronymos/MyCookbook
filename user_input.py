5517395770476e8443d2fef58cf71f65e8e72d9c
Invited User iuc6bjl6 <iuc6bjl6@mail.gitlabdemo.cloud>
2022-01-10 08:42:06 +1100

    Adding user input and variable type examples

---
+++ user_input.py
@@ -0,0 +1,9 @@
+print("hello")
+name = input("what is your name? ")
+print("hello " + name)
+
+number = input("what is your fav number? ") # Get a number from user
+triple = int(number) * 3
+print("your num multiply by three is " + str(triple))
+
+
